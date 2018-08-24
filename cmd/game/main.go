1ab701a270f021cfabb15194a0ea14e371a41501
Sebastien Carlier <s.carlier@epixode.fr>
2018-08-24 22:01:37 +0000

    fetch and save game, fetch blocks

--- cmd/game/main.go
+++ cmd/game/main.go
@@ -2,16 +2,18 @@
 package main
 
 import (
-  "path/filepath"
+  "bytes"
+  "encoding/json"
   "flag"
   "fmt"
   "io/ioutil"
   "os"
+  "path/filepath"
 
   "gopkg.in/yaml.v2"
   "tezos-contests.izibi.com/tezos-play/api"
   "tezos-contests.izibi.com/tezos-play/keypair"
-  //"tezos-contests.izibi.com/tezos-play/block_store"
+  "tezos-contests.izibi.com/tezos-play/block_store"
 )
 
 func main() {
@@ -27,6 +29,13 @@ type Config struct {
   StoreBaseUrl string `yaml:"store_base"`
   StoreCacheDir string `yaml:"store_dir"`
   WatchGameUrl string `yaml:"watch_game_url"`
+}
+
+func SaveGame(game *api.ShowGameResponse) (err error) {
+  buf := new(bytes.Buffer)
+  json.NewEncoder(buf).Encode(game)
+  err = ioutil.WriteFile("game.json", buf.Bytes(), 0644)
+  return
 }
 
 func run() error {
@@ -46,14 +55,14 @@ func run() error {
   if err != nil { return err }
 
   remote := api.New(config.ApiBaseUrl)
-  // store := block_store.New(config.StoreBaseUrl, config.StoreCacheDir)
+  store := block_store.New(config.StoreBaseUrl, config.StoreCacheDir)
 
   var teamKeyPair *keypair.KeyPair
   teamKeyPair, err = keypair.Read("team.json")
   if err != nil { return err }
 
   chainHash, err := remote.NewChain(api.TaskParams{
-      NbPlayers: 1,
+      NbPlayers: 2,
       MapSide: 11,
     },
     "val start_skeleton : int * int -> unit\nval grow_skeleton : int * int -> unit\nval echo : string -> unit",
@@ -61,15 +70,14 @@ func run() error {
   )
   if err != nil { return err }
   fmt.Fprintf(os.Stderr, "chain  %s\n", chainHash)
-
-  /*
   _, err = store.Get(chainHash)
   if err != nil { return err }
-  */
 
-  game, err := remote.NewGame(chainHash, 10, 60, 4);
+  game, err := remote.NewGame(chainHash, 10, 60, 2);
   if err != nil { return err }
   fmt.Fprintf(os.Stderr, "%s/%s\n", config.WatchGameUrl, game.Key)
+  err = SaveGame(game)
+  if err != nil { return err }
 
   err = remote.InputCommands(game.Key, 1, teamKeyPair, 1,
     "start_skeleton (0, 5); echo \"ready\ngo!\"; grow_skeleton (1, 5); grow_skeleton (2, 5)")
@@ -79,15 +87,27 @@ func run() error {
     "start_skeleton (5, 0); grow_skeleton (5, 1); echo \"thinking...\"; grow_skeleton (5, 2)")
   if err != nil { return err }
 
-  h1, err := remote.EndRound(game.Key, 1, teamKeyPair)
+  var h1 string
+  h1, err = remote.EndRound(game.Key, 1, teamKeyPair)
   if err != nil { return err }
   fmt.Fprintf(os.Stderr, "round1 %s\n", h1)
 
-/*
+  game, err = remote.ShowGame(game.Key)
+  if err != nil { return err }
+  _, err = store.Get(game.CurrentBlock)
+  if err != nil { return err }
+  err = SaveGame(game)
+  if err != nil { return err }
+
   h2, err := remote.EndRound(game.Key, 2, teamKeyPair)
   if err != nil { return err }
   fmt.Fprintf(os.Stderr, "round2 %s\n", h2)
-*/
+
+  game, err = remote.ShowGame(game.Key)
+  if err != nil { return err }
+  _, err = store.Get(game.CurrentBlock)
+  err = SaveGame(game)
+  if err != nil { return err }
 
   return nil;
 }
