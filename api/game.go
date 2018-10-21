3a857ecbcde9a3a0f69b469a56e1a32db67d0589
Sebastien Carlier <s.carlier@epixode.fr>
2018-10-21 16:16:30 +0000

    request for Game/Register API

--- api/game.go
+++ api/game.go
@@ -29,6 +29,25 @@ func (s *Server) ShowGame(gameKey string) (*GameState, error) {
   return &res.Game, nil
 }
 
+/* Register a number of players for our and retrieve their ranks in the game. */
+func (s *Server) Register(gameKey string, nbPlayers uint32) ([]uint32, error) {
+  type Request struct {
+    Author string `json:"author"`
+    NbPlayers uint32 `json:"nbPlayers"`
+  }
+  type Response struct {
+    Ranks []uint32 `json:"ranks"`
+  }
+  reqPath := fmt.Sprintf("/Games/%s/Register", gameKey)
+  var res Response
+  err := s.SignedRequest(reqPath, Request{
+    Author: s.Author(),
+    NbPlayers: nbPlayers,
+  }, &res)
+  if err != nil { return nil, err }
+  return res.Ranks, nil
+}
+
 func (s *Server) InputCommands(gameKey string, currentBlock string, teamPlayer uint32, commands string) error {
   type Request struct {
     Author string `json:"author"`
